07da6c69cae646b0fe4c4ca8ec5ff8423d3d23ca
dalienst <oduordalienst@gmail.com>
2022-06-14 12:08:56 +0300

    Changes

--- app/authentication/admin.py
+++ app/authentication/admin.py
@@ -1,5 +1,7 @@
 # Register your models here.
-from .models import User
+from django.contrib.auth import get_user_model
 from django.contrib import admin
+
+User = get_user_model()
 
 admin.site.register(User)
