00a4f20c42d2b68c5bf4626dddf14fae2fd8f4da
dalienst <oduordalienst@gmail.com>
2022-06-23 15:46:32 +0300

    fix circleci

--- speaksfer/settings/base.py
+++ speaksfer/settings/base.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third-party imports
+    "rest_framework",
+    # app imports
     "app.authentication",
 ]
 
