c6a957b971033def2c9f954de52548b576cc3fe9
dalienst <oduordalienst@gmail.com>
2022-06-26 17:08:00 +0300

    Email verification

--- app/user/urls.py
+++ app/user/urls.py
@@ -4,10 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from app.user.views import UserRegister
+from app.user.views import UserRegister, VerifyEmail
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", UserRegister.as_view(), name="register"),
+    path("email-verify/", VerifyEmail.as_view(), name="email-verify"),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,6 @@
+import jwt
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -5,6 +8,8 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from app.user.serializers import UserSerializer
+
+User = get_user_model()
 
 
 class UserRegister(APIView):
@@ -18,3 +23,26 @@ class UserRegister(APIView):
         response["access"] = str(refresh.access_token)
 
         return Response(response, status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(APIView):
+    def get(self, request: Request) -> Response:
+        token = request.GET.get("token")
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)  # type: ignore
+            user = User.objects.get(id=payload["user_id"])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response(
+                {"email": "Successfully Activated"}, status=status.HTTP_200_OK
+            )
+        except jwt.ExpiredSignatureError as identifier:  # noqa F841
+            return Response(
+                {"error": "Activation Link is expired"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        except jwt.exceptions.DecodeError as identifier:  # noqa F841
+            return Response(
+                {"error": "Invalid Token"}, status=status.HTTP_400_BAD_REQUEST
+            )
