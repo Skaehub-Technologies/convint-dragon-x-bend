40423f1ff66129edbe5f7671f2344acf29a7cfc3
dalienst <oduordalienst@gmail.com>
2022-06-29 13:42:57 +0300

    Fix failing circleCI

--- app/user/urls.py
+++ app/user/urls.py
@@ -4,11 +4,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from app.user.views import UserRegister, VerifyEmail
+from app.user.serializers import VerifyEmailSerializer
+from app.user.views import UserRegister
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", UserRegister.as_view(), name="register"),
-    path("email-verify/", VerifyEmail.as_view(), name="email-verify"),
+    path(
+        "email-verify/", VerifyEmailSerializer.as_view(), name="email-verify"
+    ),
 ]
