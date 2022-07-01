496b4eccdb8e6c285c89d0a9e48deabe06c8d934
dalienst <oduordalienst@gmail.com>
2022-07-01 11:09:21 +0300

    Tests for email verification

--- app/user/tests/test_views.py
+++ app/user/tests/test_views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from rest_framework import status
+
+from app.user.token import account_activation_token
 
 User = get_user_model()
 
@@ -29,3 +33,18 @@ class ViewsTest(TestCase):
         self.assertEqual(response.data["username"], str("foobarata"))  # type: ignore
         self.assertEqual(response.data["email"], str("foobar@example.com"))  # type: ignore
         self.assertFalse("password" in response.data)  # type: ignore
+
+    def test_new_user_verification(self) -> None:
+        new_user = User.objects.create_user(
+            "humptydumpty", "humpty@gmail.com", "felldownthewall"
+        )
+        link = reverse(
+            "email-verify",
+            kwargs={
+                "uidb64": urlsafe_base64_encode(force_bytes(new_user.pk)),
+                "token": account_activation_token.make_token(new_user),
+            },
+        )
+        resp = self.client.get(link)
+
+        self.assertEqual(resp.status_code, 200)
