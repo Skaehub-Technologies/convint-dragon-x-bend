de8c3dcb08e7e6c225586d330acd5babf9a234a9
dalienst <oduordalienst@gmail.com>
2022-07-01 12:37:39 +0300

    Change six to smart_bytes

--- app/user/token.py
+++ app/user/token.py
@@ -1,15 +1,15 @@
 from typing import Any
 
-import six
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import smart_bytes
 
 
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp) -> Any:  # type: ignore
         return (
-            six.text_type(user.pk)
-            + six.text_type(timestamp)
-            + six.text_type(user.is_verified)
+            smart_bytes(user.pk)
+            + smart_bytes(timestamp)
+            + smart_bytes(user.is_verified)
         )
 
 
