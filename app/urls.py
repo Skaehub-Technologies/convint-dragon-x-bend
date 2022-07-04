cd6b4fcf74b81cca9affc592cf43f0e04ef308df
dalienst <oduordalienst@gmail.com>
2022-07-04 12:07:59 +0300

    Set urls

---
+++ app/urls.py
@@ -0,0 +1,3 @@
+from django.urls import include, path
+
+urlpatterns = [path("user/", include("app.user.urls"))]
--- speaksfer/urls.py
+++ speaksfer/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -32,6 +32,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("app.urls")),
     path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
