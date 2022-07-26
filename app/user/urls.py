bbeca30539c7f2a7805a2277ab9f33c6b6cc41b9
Chris Onsando <chrisnemwel@gmail.com>
2022-07-26 11:45:39 +0300

    feat(users)users should be able to reset password via email (#27)
    * Password Reset Via Email

--- app/user/urls.py
+++ app/user/urls.py
@@ -5,11 +5,13 @@ from rest_framework_simplejwt.views import (
 )
 
 from app.user.views import (
+    PasswordReset,
     ProfileDetailView,
     ProfileListView,
     UserRegister,
     UserView,
     VerifyEmailView,
+    VerifyPasswordReset,
 )
 
 urlpatterns = [
@@ -17,11 +19,21 @@ urlpatterns = [
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", UserRegister.as_view(), name="register"),
     path(
-        "email-verify/<str:uidb64>/<str:token>/",
+        "email-verify/<str:encoded_pk>/<str:token>/",
         VerifyEmailView.as_view(),
         name="email-verify",
     ),
     path("profile/<str:user>/", ProfileDetailView.as_view(), name="profile"),
     path("users/", UserView.as_view(), name="users"),
     path("profiles/", ProfileListView.as_view(), name="profiles"),
+    path(
+        "password-reset/",
+        PasswordReset.as_view(),
+        name="password-reset",
+    ),
+    path(
+        "verify-password-reset/<str:encoded_pk>/<str:token>/",
+        VerifyPasswordReset.as_view(),
+        name="verify-password-reset",
+    ),
 ]
