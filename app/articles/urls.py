2e723a1c43705ed13f95642f77da90dc9f2b332c
dalienst <oduordalienst@gmail.com>
2022-08-02 09:33:46 +0300

    feat(182631402): fix failing deployment
    * update pipfile.lock to match Heroku's
    
    status: complete

--- app/articles/urls.py
+++ app/articles/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app.articles.views import ArticleDetailView, ArticleListCreateView
 
 urlpatterns = [
-    path("articles/", ArticleListCreateView.as_view(), name="article-list"),
+    path("article/", ArticleListCreateView.as_view(), name="article-list"),
     path(
         "article/<slug:slug>/",
         ArticleDetailView.as_view(),
