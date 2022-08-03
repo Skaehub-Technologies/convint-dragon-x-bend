0a5320a8ba28dcd9be70a8f78ef30cc9f7eab13f
ChrisOnsando <chrisnemwel@gmail.com>
2022-08-03 12:32:49 +0300

    feat(pagination):configure pagination

--- speaksfer/settings/base.py
+++ speaksfer/settings/base.py
@@ -156,6 +156,8 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.MultiPartRenderer",
         "rest_framework.renderers.JSONRenderer",
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 2,
 }
 
 SIMPLE_JWT = {
