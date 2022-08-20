7283fe7d86c38bc9663ef1d2fde1c427d6eca29d
ChrisOnsando <chrisnemwel@gmail.com>
2022-08-20 14:05:49 +0300

    articles

--- app/articles/filters.py
+++ app/articles/filters.py
@@ -3,6 +3,7 @@ from django_filters import FilterSet
 
 from app.articles.models import Article
 
+
 class ArticleFilter(FilterSet):  # type:ignore[no-any-unimported]
     author = django_filters.CharFilter(
         field_name="author__username", lookup_expr="icontains"
@@ -10,7 +11,7 @@ class ArticleFilter(FilterSet):  # type:ignore[no-any-unimported]
     tags = django_filters.CharFilter(
         field_name="tags__name", lookup_expr="icontains"
     )
-    title = django_filters.CharFilter(lookup_expr='iexact')
+    title = django_filters.CharFilter(lookup_expr="iexact")
 
     class Meta:
         model = Article
