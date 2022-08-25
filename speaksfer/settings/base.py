02d4e54550568416823767c833179a5a1d7aad8a
Chris Onsando <chrisnemwel@gmail.com>
2022-08-25 11:02:02 +0300

    feat(articles): users be able to see their articles reading stats(#45)
    * users should be able to see their articles reading stats

--- speaksfer/settings/base.py
+++ speaksfer/settings/base.py
@@ -65,16 +65,16 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
 # White listing the localhost:3000 port
-CORS_ORIGIN_WHITELIST = "http://localhost:3000"
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
 ROOT_URLCONF = "speaksfer.urls"
 
 TEMPLATES = [
