ab9a64548f73d65637d6412d455820f0d87eb21f
dalienst <oduordalienst@gmail.com>
2022-09-03 18:28:49 +0300

    fix cors allow credentials

--- speaksfer/settings/base.py
+++ speaksfer/settings/base.py
@@ -77,6 +77,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+CORS_ALLOW_CREDENTIALS = True
 # White listing the localhost:3000 port
 CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
 ROOT_URLCONF = "speaksfer.urls"
