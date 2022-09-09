9e2102843f4c198c126cd6eb59d08791d28ee5e6
dalienst <oduordalienst@gmail.com>
2022-09-09 11:16:27 +0300

    set allow origins

--- speaksfer/settings/base.py
+++ speaksfer/settings/base.py
@@ -86,6 +86,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:3000",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "https://speaksfer.netlify.app",
     "http://localhost:3000",
